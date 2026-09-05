ddd0950d366538b445403499606b0e30ed5c6239
Nishant Tyagi <Nishanttyagi1080@gmail.com>
2026-09-05 22:33:21 +0530

    IteratorFramework

--- force-app/main/default/lwc/iteratorFramework/iteratorFramework.js
+++ force-app/main/default/lwc/iteratorFramework/iteratorFramework.js
@@ -1,3 +1,10 @@
 import { LightningElement } from 'lwc';
 
-export default class IteratorFramework extends LightningElement {}
+export default class IteratorFramework extends LightningElement {
+
+    taskList = [
+         { taskId: 1, taskName: "Start Coding in VS Code", taskPriority:"High",taskProgress:"In Progress"},
+         { taskId: 2, taskName: "Learn LWC Concept", taskPriority:"Medium",taskProgress:"Pending"},
+         { taskId: 3, taskName: "Practice LWC", taskPriority:"Low",taskProgress:"Pending"}
+    ]
+}
